c4c1f711c68d92e6b8b007136d4e426a6c614e4d
Rosmer <criptopreto@gmail.com>
2021-08-16 23:51:50 -0400

    Fix

--- database/crud.py
+++ database/crud.py
@@ -41,7 +41,7 @@ def get_data_pair(pair_name: str, interval: str):
 
 def get_last_data_pair(pair_name: str, interval: str):
     try:
-        query = db_pair[f"{pair_name}_{interval}"].find().sort({'_id': DESCENDING}).limit(1)
+        query = db_pair[f"{pair_name}_{interval}"].find().sort({'_id', DESCENDING}).limit(1)
         print(query)
     except Exception as e:
         print("Errir get last data pair", e)
